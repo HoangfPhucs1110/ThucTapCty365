790fea16ce399835ccfb459b87ac302998d0d702
HoangfPhucs1110 <2200001472@nttu.edu.vn>
2025-10-02 16:31:12 +0700

    add ngay3

---
+++ Tuan1/Ngay3/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default App
